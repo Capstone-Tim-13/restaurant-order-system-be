7e72af0bf4790a26ae3458bb57d7aad4a0c10452
Muhammad Sidik <77959250+msidik17@users.noreply.github.com>
2023-11-07 16:53:18 +0700

    default setup

---
+++ database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"capstone/config"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	config := config.LoadDBConfig()
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DB_USER, config.DB_PASS, config.DB_HOST, config.DB_PORT, config.DB_NAME)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	migrate(db)
+
+	return db
+}
+
+func migrate(db *gorm.DB) {
+	db.AutoMigrate()
+}
