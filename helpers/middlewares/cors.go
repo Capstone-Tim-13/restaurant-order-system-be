609d48c444f5c404e252ce8e9391ae4ef94d530a
Muhammad Sidik <77959250+msidik17@users.noreply.github.com>
2023-11-23 14:49:47 +0700

    Update cors.go

--- helpers/middlewares/cors.go
+++ helpers/middlewares/cors.go
@@ -9,6 +9,6 @@ import (
 func SetupCORS(app *echo.Echo) {
 	app.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
-		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodPatch, http.MethodOptions},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodPatch},
 	}))
 }
