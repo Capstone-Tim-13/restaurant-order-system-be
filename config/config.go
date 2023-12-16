3d1903d19672caa06d03d53fc28a8680f58132b4
Muhammad Sidik <77959250+msidik17@users.noreply.github.com>
2023-12-16 16:11:54 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -3,8 +3,6 @@ package config
 import (
 	"os"
 	"strconv"
-
-	"github.com/joho/godotenv"
 )
 
 type DatabaseConfig struct {
@@ -25,7 +23,7 @@ type ServerConfig struct {
 }
 
 func LoadDBConfig() DatabaseConfig {
-	godotenv.Load(".env")
+	//godotenv.Load(".env")
 
 	DB_PORT, err := strconv.Atoi(os.Getenv("DB_PORT"))
 
